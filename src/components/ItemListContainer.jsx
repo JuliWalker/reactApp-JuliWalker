54680f2ed9f1d1e068bc744ce56ec5a23a5f8939
JuliWalker <julianwalker1992@gmail.com>
2022-01-18 22:51:10 -0300

    agrego las modificaciones de la clase 6

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,17 +3,47 @@ import { useState } from "react";
 import { useEffect } from "react";
 import ItemList from "./ItemList";
 
-export default function ItemListContainer({}) {
-    
-    const [arrayProductos, setArrayProductos] = useState ([
-        {id: '1', title: 'Lompa Leopardo', description: 'pantalon con motivo de leopardo', price: 300, pictureURL: '', stock: 10},
-        {id: '2', title: 'Medias Flama', description: 'medias con dibujos de llamitas de fuego', price: 100, pictureURL: '', stock: 5},
-        {id: '3', title: 'Gorra LA', description: 'gorra de beisbol Los Angeles Angels', price: 200, pictureURL: '', stock: 7}
-    ]);
+export default function ItemListContainer({ }) {
+
+    const [llegoPromesa, setLlegoPromesa] = useState(false);
+    const [arrayProductos, setArrayProductos] = useState([]);
+
+
+    useEffect(() => {
+
+        const promesaProductos = new Promise((resolve, reject) => {
+
+            setTimeout(() => {
+                resolve([{ id: '1', title: 'Lompa Leopardo', description: 'pantalon con motivo de leopardo', price: 300, pictureURL: '', stock: 10 },
+                { id: '2', title: 'Medias Flama', description: 'medias con dibujos de llamitas de fuego', price: 100, pictureURL: '', stock: 5 },
+                { id: '3', title: 'Gorra LA', description: 'gorra de beisbol Los Angeles Angels', price: 200, pictureURL: '', stock: 7 }]);
+            }, 2000)
+        });
+
+        promesaProductos
+            .then((res) => {
+                setLlegoPromesa(true);
+                setArrayProductos(res);
+            })
+            .catch((err) => {
+                setLlegoPromesa(true);
+                console.log(err);
+            })
+    });
 
     return (
         <>
-            < ItemList arrayProductos={arrayProductos} />
+            {(llegoPromesa) ?
+                <>
+                    <div className="container mt-5 text-center">
+                        <div className="row justify-content-evenly">
+                            < ItemList arrayProductos={arrayProductos} />
+                        </div>
+                    </div>
+                </>
+                :
+                <>Loading...</>
+            }
         </>
     )
 
